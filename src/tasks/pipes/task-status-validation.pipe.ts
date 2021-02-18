7980ac7b033df9cffbea40938f1b66bae88d02df
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-02-18 07:09:36 +0200

    handle validation

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusvalidationPipe implements PipeTransform {
+  readonly allowedSatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+    if (!this.isStatusvalid(value)) {
+      throw new BadRequestException(`${value} is an invalid status`);
+    }
+    return value;
+  }
+
+  private isStatusvalid(status: any) {
+    const idx = this.allowedSatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { TaskStatusvalidationPipe } from './pipes/task-status-validation.pipe';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) { }
+
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilter(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status', TaskStatusvalidationPipe) status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
