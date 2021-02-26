23b5d7cc0c94455f436c39af05caed821f623d85
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-02-26 08:22:41 +0200

    Security hashing password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,16 +1,21 @@
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from "./dto/auth-credentials-dto";
 import { User } from "./user.entity";
+
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
   async signUp(authCredendialsDto: AuthCredentialsDto): Promise<void> {
-    const { username, password } = authCredendialsDto
-    const user = new User()
-    user.username = username
-    user.password = password
+    const { username, password } = authCredendialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
 
     try {
       await user.save();
@@ -23,5 +28,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
