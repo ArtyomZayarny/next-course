00de8e0c7d310f295fe04d1106de01a310ca06af
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-03-02 07:22:55 +0200

    Authorization get all task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,8 +14,11 @@ export class TasksService {
     private taskRepository: TaskRepository
   ) { }
 
-  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
-    return this.taskRepository.getTasks(filterDto)
+  async getTasks(
+    filterDto: GetTasksFilterDto,
+    user: User,
+  ): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto, user)
   }
 
   async createTask(
